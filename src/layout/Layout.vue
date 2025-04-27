2953b5f4e3855d60f54bb14b361c1b6d1d352b93
nebula <jing.me@icloud.com>
2025-04-27 10:01:23 +0800

    style: 静态资源文件

---
+++ src/layout/Layout.vue
@@ -0,0 +1,44 @@
+<script setup lang="ts">
+
+interface PropsType {
+  load: {
+    total: number
+    loaded: number
+    isLading: boolean
+  }
+}
+defineProps<PropsType>()
+</script>
+
+<template>
+  <div class="layout">
+    <div class="layout-main">
+      <div class="layout-left">
+        <slot name="left" />
+      </div>
+      <div class="layout-right">
+        <slot name="right" />
+      </div>
+      <div class="layout-center">
+        <slot/>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.layout {
+  width: 100%;
+  height: 100%;
+  background: #000;
+
+  .layout-main {
+    position: relative;
+    width: 100%;
+    height: calc(100% - 80px);
+    background-color: #05326a;
+    background-image: url('@/assets/images/bg_01.png');
+    background-repeat: repeat;
+  }
+}
+</style>
