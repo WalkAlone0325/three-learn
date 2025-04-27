b59dea9e946dc46af10c28f17991f82b15f2bab0
nebula <jing.me@icloud.com>
2025-04-27 09:46:43 +0800

    chore: init

--- src/main.ts
+++ src/main.ts
@@ -2,5 +2,15 @@ import './assets/main.css'
 
 import { createApp } from 'vue'
 import App from './App.vue'
+import autofit from 'autofit.js'
 
-createApp(App).mount('#app')
+const app = createApp(App)
+app.mount('#app')
+
+// 自适应
+autofit.init({
+  el: '#app',
+  dw: 1920,
+  dh: 1080,
+  resize: true,
+})
